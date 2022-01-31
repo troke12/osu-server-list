ab4a1e63c2cb044fd1449c4d8a8b79ee1140cfc9
I Made Ocy Darma Putra <jame_glove@yahoo.com>
2022-01-31 14:17:37 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,19 @@
+# Osu! Private Server Listing
+
+The list of osu private server
+
+#### How to add your server
+
+<pre>
+var items = [
+  {
+    name: "Name of Your Server",
+    link: "http://yourosuserver",
+    owner: "your owner",
+    location: "City or country",
+    discord: "Discord Invite"
+  }
+];
+</pre>
+
+Please make sure each of your items is separate.
---
+++ data.js
@@ -0,0 +1,58 @@
+var items = [
+  {
+    name: "DATENSHI",
+    link: "https://osu.datenshi.pw",
+    owner: "troke",
+    location: "Indonesia",
+    discord: "https://datenshi.pw/discord"
+  },
+  {
+    name: "Ripple",
+    link: "https://ripple.moe",
+    owner: "Nyo",
+    location: "Italy",
+    discord: "https://discord.com/invite/0rJcZruIsA6rXuIx"
+  },
+  {
+    name: "RealistikOsu",
+    link: "https://ussr.pl",
+    owner: "RealistikDash",
+    location: "United Kingdom",
+    discord: "https://discord.gg/8ySdzhyMtt"
+  },
+  {
+    name: "EZPPFarm",
+    link: "https://ez-pp.farm",
+    owner: "Quetzalcoatl, Thomas",
+    location: "Germany",
+    discord: "https://discord.gg/g8Bh7RaKPg"
+  },
+  {
+    name: "Fuquila",
+    link: "https://fuquila.net",
+    owner: "Aochi",
+    location: "Australia",
+    discord: "https://discord.gg/zEeU85Kf5r"
+  },
+  {
+    name: "Fumosu",
+    link: "https://fumosu.pw",
+    owner: "Aochi",
+    location: "United States",
+    discord: "https://discord.gg/SvE65GsEXE"
+  },
+  {
+    name: "Miksu",
+    link: "https://miku.network",
+    owner: "Sensokaku",
+    location: "United States",
+    discord: "https://discord.gg/DPcJP9CtdC"
+  },
+  {
+    name: "Masayuki",
+    link: "https://masayuki.cf",
+    owner: "Hitoriono",
+    location: "United States",
+    discord: "https://discord.gg/EQcvGMjDYP"
+  },
+];
