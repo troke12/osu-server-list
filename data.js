f7e3477d895949671d449f36af43cbab3bb1f847
troke12 <jame_glove@yahoo.com>
2022-04-01 21:21:25 +0800

    new improvements, add online users in every server

--- data.js
+++ data.js
@@ -4,6 +4,8 @@ var items = [
     link: "https://osu.datenshi.pw",
     owner: "troke",
     location: "Indonesia",
+    onlineAPI: "https://c.datenshi.pw/api/v1/onlineUsers",
+    instances: 0,
     discord: "https://datenshi.pw/discord"
   },
   {
@@ -11,20 +13,17 @@ var items = [
     link: "https://tski.moe",
     owner: "jupiter (j@upiter.one, jupiter#2132)",
     location: "Australia",
+    onlineAPI: "https://api.tski.moe/get_player_count",
+    instances: 1,
     discord: "https://tski.moe/discord"
-  },
-  {
-    name: "Ripple",
-    link: "https://ripple.moe",
-    owner: "Nyo",
-    location: "Italy",
-    discord: "https://discord.com/invite/0rJcZruIsA6rXuIx"
   },
   {
     name: "RealistikOsu",
     link: "https://ussr.pl",
     owner: "RealistikDash",
     location: "United Kingdom",
+    onlineAPI: "https://c.ussr.pl/api/v1/onlineUsers",
+    instances: 0,
     discord: "https://discord.gg/8ySdzhyMtt"
   },
   {
@@ -32,6 +31,8 @@ var items = [
     link: "https://ez-pp.farm",
     owner: "Quetzalcoatl, Thomas",
     location: "Germany",
+    onlineAPI: "https://c.ez-pp.farm/api/v1/onlineUsers",
+    instances: 0,
     discord: "https://discord.gg/g8Bh7RaKPg"
   },
   {
@@ -39,6 +40,8 @@ var items = [
     link: "https://fuquila.net",
     owner: "Aochi, Sensokaku",
     location: "Australia",
+    onlineAPI: "https://c.fuquila.net/api/v1/onlineUsers",
+    instances: 0,
     discord: "https://discord.gg/zEeU85Kf5r"
   },
   {
@@ -46,6 +49,8 @@ var items = [
     link: "https://fumosu.pw",
     owner: "Aochi",
     location: "United States",
+    instances: 1,
+    onlineAPI: "https://api.fumosu.pw/get_player_count",
     discord: "https://discord.gg/SvE65GsEXE"
   },
   {
@@ -53,27 +58,35 @@ var items = [
     link: "https://miku.network",
     owner: "Sensokaku",
     location: "United States",
+    onlineAPI: "https://api.miku.network/get_player_count",
+    instances: 1,
     discord: "https://discord.gg/DPcJP9CtdC"
   },
   {
-    name: "Masayuki",
-    link: "https://masayuki.cf",
-    owner: "Hitoriono",
-    location: "United States",
-    discord: "https://discord.gg/EQcvGMjDYP"
-  },
-  {
-    name: "osu!Aeris",
-    link: "https://aeris-dev.pw/",
-    owner: "PANIGE",
-    location: "France",
-    discord: "https://discord.gg/GZwWzQZ"
+    name: "Kurikku",
+    link: "https://kurikku.pw",
+    owner: "Wenin",
+    location: "Russia",
+    onlineAPI: "https://c.kurikku.pw/api/v1/onlineUsers",
+    instances: 0,
+    discord: "https://discord.com/invite/5uA3c76"
   },
   {
     name: "Rina",
     link: "https://rina.place",
     owner: "Aoba Suzukaze",
     location: "Finland",
+    onlineAPI: "https://c.rina.place/api/v1/onlineUsers",
+    instances: 0,
     discord: "https://discord.gg/Qp3WQU8"
+  },
+  {
+    name: "Akatsuki",
+    link: "https://akatsuki.pw",
+    owner: "CursorDance",
+    location: "United States",
+    onlineAPI: "https://c.akatsuki.pw/api/v1/onlineUsers",
+    instances: 0,
+    discord: "https://akatsuki.pw/discord"
   }
 ];
